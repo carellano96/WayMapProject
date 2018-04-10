922c7a5817b56ed0a35565e901cde564ae4b6f18
carellano96 <carellano96@gmail.com>
2018-04-10 00:31:49 -0400

    Add files via upload
    Added

---
+++ WayMap/WayMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WayMap
+//
+//  Created by carlos arellano on 4/5/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WayMap/WayMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WayMap
+//
+//  Created by carlos arellano on 4/5/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
