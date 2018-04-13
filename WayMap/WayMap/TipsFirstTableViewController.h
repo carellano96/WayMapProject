1acf4792748109e3f01330169086a612f14f60b3
carellano96 <carlos@172-18-44-7.DYNAPOOL.NYU.EDU>
2018-04-12 23:31:01 -0400

    added more stuff

---
+++ WayMap/WayMap/TipsFirstTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipsFirstTableViewController.h
+//  WayMap
+//
+//  Created by carlos arellano on 4/12/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipsFirstTableViewController : UITableViewController
+
+@end
