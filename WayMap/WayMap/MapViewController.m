72507a13d343555930fbb534c7fa446da50cb45c
carlos arellano <carlos@MacBook-Pro-7.local>
2018-04-15 16:14:17 -0400

    Even more stuff added

--- WayMap/WayMap/MapViewController.m
+++ WayMap/WayMap/MapViewController.m
@@ -23,6 +23,7 @@
     [self mapView:MapView didSelectUserLocation:MapView.userLocation];
     // Do any additional setup after loading the view.
     //Fun new stuff
+    NSLog(@"New stuff added too!");
 }
 
 - (void)mapView:(MGLMapView *)mapView didSelectUserLocation:(MGLUserLocation*)location {
