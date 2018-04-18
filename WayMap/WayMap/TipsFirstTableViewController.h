e259b8dbd20ab4f03b59bf8b396a675f6941dded
carlos arellano <carlos@172-18-47-63.DYNAPOOL.NYU.EDU>
2018-04-18 00:13:42 -0400

    Categorized different stuff and put into relevant table views, Categories appear only if location is available

--- WayMap/WayMap/TipsFirstTableViewController.h
+++ WayMap/WayMap/TipsFirstTableViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TipsSecondTableViewController.h"
 @import Mapbox;
 @import MapKit;
 @import GooglePlaces;
@@ -14,7 +15,23 @@
 @interface TipsFirstTableViewController : UITableViewController <UITabBarControllerDelegate,UITabBarDelegate>
 @property (strong,nonatomic) NSMutableArray*categories;
 @property (strong) GMSPlaceLikelihoodList* LikelyList;
+@property (strong,nonatomic) NSString*LocationName;
+@property (strong,nonatomic) NSMutableArray*Food;
+@property (strong,nonatomic) NSMutableArray*Leisure;
+@property (strong,nonatomic) NSMutableArray*Entertainment;
+@property (strong,nonatomic) NSMutableArray*Culture;
+@property (strong,nonatomic) NSMutableArray*Other;
+@property (strong,nonatomic) NSMutableArray*Shopping;
+@property (strong,nonatomic) NSMutableArray*Transportation;
+@property (strong,nonatomic) NSMutableArray*Financial;
+@property (strong,nonatomic) NSMutableArray*Occupational;
+@property (strong,nonatomic) NSMutableArray*ServicesOther;
+@property (strong,nonatomic) NSMutableArray*Lifestyle;
+@property (strong,nonatomic) TipsSecondTableViewController*Tips1;
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+- (void) CategorizeLocations
+:(GMSPlaceLikelihoodList*)LikelyList;
 @end
